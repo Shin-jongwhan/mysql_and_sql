1df34a5bf10fc87e40af49308d52ff00151aa12a
Shin-jonghwan <62974484+Shin-jongwhan@users.noreply.github.com>
2022-11-11 02:23:17 +0900

    a

--- mysql/mysql_test_221111.py
+++ mysql/mysql_test_221111.py
@@ -2,12 +2,18 @@ import pymysql
 
 # STEP 2: MySQL Connection 연결
 con = pymysql.connect(host='localhost', user='root', password='root',
-                       db='world')
+                       db='test')
  
 cur = con.cursor()
-sql = "show tables;"
+
+# 행 추가
+sql = "insert into test (ID, name, age) VALUES (3, 'python', '5');"
 cur.execute(sql)
- 
+rows = cur.fetchall()
+con.commit()        # db 에 반영
+
+sql = "select * from test;"
+cur.execute(sql)
 rows = cur.fetchall()
 print(rows)
 
--- mysql/sql_test_221111.sql
+++ mysql/sql_test_221111.sql
@@ -1,4 +1,18 @@
--- Active: 1668098179821@@127.0.0.1@3306@world
--- show databases;
 show tables;
 show databases;
+-- create database test;
+use test;
+/* create table test (
+    ID int not null,
+    name varchar(20) not null,
+    age int not null,
+    address char(50),
+    salary decimal (18, 2),
+    PRIMARY key(ID)
+);
+*/
+desc test;
+insert into test (ID, name, age) VALUES (1, 'jonghwan', '29');
+-- OR
+insert into test VALUES (2, 'jonghwan', '29', NULL, NULL);
+select * from test;

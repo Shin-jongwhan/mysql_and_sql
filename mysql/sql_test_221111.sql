32d5866c6a56dd9eab650f94b1988b2153bc9143
Shin-jonghwan <62974484+Shin-jongwhan@users.noreply.github.com>
2022-11-14 09:55:55 +0900

    Update sql_test_221111.sql

--- mysql/sql_test_221111.sql
+++ mysql/sql_test_221111.sql
@@ -16,3 +16,9 @@ insert into test (ID, name, age) VALUES (1, 'jonghwan', '29');
 -- OR
 insert into test VALUES (2, 'jonghwan', '29', NULL, NULL);
 select * from test;
+
+-- index AUTO_INCREMENT 를 통해 자동으로 max 값을 넣어줄 수 있도록 한다.
+CREATE TABLE 'test'(
+'num' int(10) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+'name' varchar(10) NOR NULL
+);
